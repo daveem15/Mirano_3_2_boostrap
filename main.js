9d418763113da763cb9af9fd5f056db222c45ac6
David Mirano <davidmirano55@gmail.com>
2023-11-01 18:38:55 +0800

    latest

---
+++ main.js
@@ -0,0 +1,7 @@
+let iconCart = document.querySelector('.icons');
+let body = document.querySelector('body');
+
+ icons.addEventListener('click', () => {
+    body.clastList.toggle('showCart')
+ })
+
