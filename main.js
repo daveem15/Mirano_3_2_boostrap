b4f3443fb307db8721c74b8131b74509a7770b3d
daveem15 <144328345+daveem15@users.noreply.github.com>
2023-11-16 14:40:22 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//console.log("Hello World");
+
+//let name = "David";
+//let age = 20;
+//let isSingle = true;
+//let hobbies = ["Playing Volleyball", "Watching Movies", 1, true];
+
+//let myObject = {
+  // name,
+   //age,
+   //isSingle,
+   //hobbies
+//};
+
+
