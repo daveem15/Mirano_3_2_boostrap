7fb590c0968553015f2855506d1ce2811c28a236
daveem15 <144328345+daveem15@users.noreply.github.com>
2023-11-16 14:38:37 +0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let iconCart = document.querySelector('.icons');
-let body = document.querySelector('body');
-
- icons.addEventListener('click', () => {
-    body.clastList.toggle('showCart')
- })
-
